eee1cb4a0e838484b2ee46e45b791cd19db6dde6
Rostyslav <rost.shat@gmail.com>
2019-12-12 22:11:42 +0200

    5-1

--- 5/5/5/Source.cpp
+++ 5/5/5/Source.cpp
@@ -16,7 +16,7 @@ int main()
 		cout << "3 - Deposit calculator\n";
 		cout << "4 - Glasn\n";
 		cout << "5 - Symbol Line\n";
-		cout << "6 - Transist line\n"; //ÃŠÃ®Ã°Ã°Ã¥ÃªÃ²Ã­Ã® Ã­Ã Ã§Ã¢Ã Ã²Ã¼
+		cout << "6 - Transist line\n";
 		cout << "0 - Exit\n";
 
 		cin >> taskNum;
@@ -26,6 +26,37 @@ int main()
 		{
 			case '1':
 			{
+				const int MAXSIZE = 20;
+				const int MAX_MARK = 5;
+				const int MIN_MARK = 1;
+
+				int marks[MAXSIZE] = {};
+
+				for (int i = 0; i < MAXSIZE; i++)
+				{
+					marks[i] = rand() % (MAX_MARK - MIN_MARK + 1) + MIN_MARK;
+				}
+				
+				for (int i = 1; i <= MAX_MARK; i++)
+				{
+					int temp{};
+					for (int j = 0; j < MAXSIZE; j++)
+					{
+						if (marks[j] == i) temp++;
+					}
+					cout << i << " : " << temp << endl;
+				}
+			}break;
+			case '2':
+			{
+
+			}break;
+			case '3':
+			{
+
+			}break;
+			case '4':
+			{
 
 			}break;
 			default:
@@ -33,6 +64,7 @@ int main()
 
 			}break;
 		}
+		system("pause");
 	}
 
 	return 0;
