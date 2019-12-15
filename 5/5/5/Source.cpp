4f316964fe9eac45854d671f417363b195e4c5c9
Rostyslav <rost.shat@gmail.com>
2019-12-15 10:34:59 +0200

    5-5_start
    5-1
    5-2
    5-3
    5-4 SKIPPED
    5-5 start

--- 5/5/5/Source.cpp
+++ 5/5/5/Source.cpp
@@ -1,9 +1,11 @@
 ﻿#include <iostream>
 #include <time.h>
+#include <iomanip>
 using namespace std;
 
 int getRandomInt(int min = 0, int max = 100);
 int getRandomIntRB(int min = 0, int max = 100);
+float getPercentage(int all_percent, int number);
 
 int main()
 {
@@ -14,8 +16,8 @@ int main()
 	{
 		system("cls");
 		cout << "Choise your task: \n";
-		cout << "1 - V of cylinder \n";
-		cout << "2 - Max of 3 numbers \n";
+		cout << "1 - Student`s marks\n";
+		cout << "2 - 15 goods \n";
 		cout << "3 - Deposit calculator\n";
 		cout << "4 - Glasn\n";
 		cout << "5 - Symbol Line\n";
@@ -29,7 +31,41 @@ int main()
 		{
 			case '1':
 			{
-					
+				const int MIN_MARK = 1;
+				const int MAX_MARK = 5;
+				const int MARKS_PER_STUDENT= 1;
+
+				int marks[][1] = { 0 };
+				int numberOfMarks{};
+
+				do
+				{
+					cout << "Enter your number of marks (more than 20): ";
+					cin >> numberOfMarks;
+				} while (numberOfMarks < 20);
+
+
+				/*GENERATE MARKS*/
+				for (int i = 0; i < numberOfMarks; i++)
+				{
+					for (int j = 0; j < MARKS_PER_STUDENT; j++)
+					{
+						marks[i][j] = getRandomIntRB(MIN_MARK, MAX_MARK);
+					}
+				}
+
+				/*OUTPUT MARKS*/
+
+				for (int i = 1; i <= MAX_MARK; i++)
+				{
+					int temp{};
+					int sumTemp{};
+					for (int j = 0; j < numberOfMarks; j++)
+					{
+						if(marks[j][0] == i) temp++;
+					}
+					cout << setw(4) << fixed << setprecision(3) << i << " : " << temp << " : " << getPercentage(numberOfMarks, temp) << '%' << endl;
+				}
 			}break;
 			case '2':
 			{
@@ -37,6 +73,7 @@ int main()
 				const int MIN_PRICE = 20;
 				const int MAX_PRICE = 120;
 				const int ATTRIBUTES = 1;
+				const int FORMAT = 4;
 
 				int goods[15][1] = { 0 };
 				int temp{}, average{};
@@ -67,16 +104,80 @@ int main()
 					{
 						if (goods[i][j] < average)
 						{
-							cout << i << " : " << goods[i][j] << endl;
+							cout << setw(FORMAT) << i + 1 << " : " << goods[i][j] << endl;
 						}
 					}
 				}
 			}break;
 			case '3':
 			{
+				const int NUMBER_OF_GOODS = 10;
+				const int MIN_PRICE = 10;
+				const int MAX_PRICE = 75;
+				const int ATTRIBUTES = 1;
+				const int FORMAT = 4;
+
+				int goods[10][1] = { 0 };
+				int temp{}, average{}, sumTemp{};
+
+				/*Generate prices*/
+				for (int i = 0; i < NUMBER_OF_GOODS; i++)
+				{
+					for (int j = 0; j < ATTRIBUTES; j++)
+					{
+						goods[i][j] = getRandomIntRB(MIN_PRICE, MAX_PRICE);;
+					}
+				}
+
+				/*Goods < 40*/
+				for (int i = 0; i < NUMBER_OF_GOODS; i++)
+				{
+					for (int j = 0; j < ATTRIBUTES; j++)
+					{
+						if (goods[i][j] < 40)
+						{
+							temp++;
+							sumTemp += goods[i][j];
+
+							cout << "Good #" << setw(2) << i + 1 << " costs " << goods[i][j] << endl;
+						}
+					}
+				}
+				cout << endl << "Number of goods which cost under 40: " << temp << endl;
+				cout << "Average price of goods which cost under 40: " << sumTemp / temp << endl;
 
 			}break;
 			case '4':
+			{
+
+			}break;
+			case '5':
+			{
+				/*
+					const int ROWS = 10;
+					const int COLS = 10;
+
+					int array[ROWS][COLS] = { 0 };
+
+					for (int i = 0; i < ROWS; i++)
+					{
+						array[i] = getRandomIntRB(-10, 10);
+						for (int j = 0; j < COLS; j++)
+						{
+							array[i][j] = getRandomIntRB(-10, 10);
+						}
+					}
+				*/
+			}break;
+			case '6':
+			{
+
+			}break;
+			case '7':
+			{
+
+			}break;
+			case '8':
 			{
 
 			}break;
@@ -99,4 +200,8 @@ int getRandomInt(int min, int max)
 int getRandomIntRB(int min, int max)
 {
 	return rand() % (max - min + 1) + min;
+}
+float getPercentage(int all_percent, int number)
+{
+	return (float)number / (float)all_percent * 100;
 }
