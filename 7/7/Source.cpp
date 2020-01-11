76a990a89bc399e7e710aa961c19b5325bcd6d7f
Rostyslav <rost.shat@gmail.com>
2020-01-11 10:34:10 +0200

    7-2 finished

--- 7/7/Source.cpp
+++ 7/7/Source.cpp
@@ -6,6 +6,8 @@ using namespace std;
 template <typename T>
 T getRandom(T min, T max, int order = 1);
 
+#define ARRAY_START 0
+#define WRAP cout << endl
 
 template <typename T>
 void swap(T* xp, T* yp);
@@ -21,7 +23,10 @@ template <typename T>
 void showArray(T* array, const int rows, const int cols);
 
 template <typename T>
-void bubbleSort(T* array, const int size, const int firstIndex, const int lastIndex);
+void bubbleSortDescending(T* array, const int size, const int firstIndex, const int lastIndex);
+
+template <typename T>
+void bubbleSortAscending(T* array, const int size, const int firstIndex, const int lastIndex);
 
 int main()
 {
@@ -32,13 +37,11 @@ int main()
 	{
 		system("cls");
 		cout << "Choise your task: \n";
-		cout << "1 - Volume of figures\n";
-		cout << "2 - Matrix Generator \n";
-		cout << "3 - Show Matrix\n";
-		cout << "4 - Diagonal Summ\n";
-		cout << "5 - Max number in P\n";
-		cout << "6 - Average number\n";
-		cout << "0 - Exit\n";
+		cout << "1 -	-20 to 20 Bubble Sort\n";
+		cout << "2 -	10.00 to 99.99 Half Sort\n";
+		cout << "3 -	-20 to 20 Bubble Sort\n";
+		cout << "4 -	-20 to 20 Bubble Sort\n";
+		cout << "0 -	Exit\n";
 
 		cin >> taskNumber;
 		if (taskNumber == '0') break;
@@ -52,16 +55,55 @@ int main()
 				const int MAX = 20;
 
 				int array[15] = { 0 };
+				int smallestIndex = 0;
+				int biggestIndex = 0;
 
 				fillArray(array, ARR_SIZE, MIN, MAX);
 				showArray(array, ARR_SIZE);
-				bubbleSort(array, ARR_SIZE, 0,ARR_SIZE);
+
+
+				for (int i = 0; i < ARR_SIZE; i++)
+				{
+					if (array[i] < 0)
+					{
+						smallestIndex = i;
+						break;
+					}
+				}
+
+				for (int i = ARR_SIZE - 1; i >= 0; i--)
+				{
+					if (array[i] < 0)
+					{
+						biggestIndex = i;
+						break;
+					}
+				}
+
+				cout << "The smallest < 0 index: " << smallestIndex << endl;
+				cout << "The biggest < 0 index: "<< biggestIndex << endl;
+
+				bubbleSortDescending(array, ARR_SIZE, smallestIndex + 1, biggestIndex - 1);
 				showArray(array, ARR_SIZE);
 
 				break;
 			}
 			case '2':
 			{
+				const short ARR_SIZE = 20;
+				const double MIN = 10.00;
+				const double MAX = 99.99;
+
+				double array[ARR_SIZE] = { 0 };
+
+				fillArray(array, ARR_SIZE, MIN, MAX, 100);
+				showArray(array, ARR_SIZE);
+				WRAP;
+				bubbleSortAscending(array, ARR_SIZE, ARRAY_START, ARR_SIZE / 2);
+				showArray(array, ARR_SIZE);
+				WRAP;
+				bubbleSortDescending(array, ARR_SIZE, ARR_SIZE / 2, ARR_SIZE);
+				showArray(array, ARR_SIZE);
 
 				break;
 			}
@@ -147,13 +189,31 @@ void swap(T* xp, T* yp)
 }
 
 template <typename T>
-void bubbleSort(T* array, const int size, const int firstIndex, const int lastIndex)
+void bubbleSortDescending(T* array, const int size, const int firstIndex, const int lastIndex)
 {
-	for (int i = firstIndex; i < lastIndex; i++) 
+	for (int i = firstIndex; i < lastIndex; i++)
 	{
-		for (int j = lastIndex - 1; j > i; j--) 
+		for (int j = firstIndex; j < lastIndex; j++)
 		{
-			if (array[j - 1] > array[j]) swap(&array[j], &array[j + 1]);
+			if (array[j] < array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+			}
+		}
+	}
+}
+
+template <typename T>
+void bubbleSortAscending(T* array, const int size, const int firstIndex, const int lastIndex)
+{
+	for (int i = firstIndex; i < lastIndex; i++)
+	{
+		for (int j = firstIndex; j < lastIndex; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+			}
 		}
 	}
 }
