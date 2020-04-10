173b9b77b7cce688e85e6f3114f3f51a98c0c521
Rostyslav <rost.shat@gmail.com>
2020-04-10 19:17:00 +0300

    Try 1

--- OOP_homework/OOP_homework/Source.cpp
+++ OOP_homework/OOP_homework/Source.cpp
@@ -4,6 +4,8 @@ class Fraction {
 	int numerator;
 	int denominator;
 public:
+	friend Fraction operator+ (const Fraction& first, const Fraction second);
+
 	Fraction() {
 		numerator = 1;
 		denominator = 1;
@@ -12,6 +14,15 @@ public:
 		numerator = first;
 		denominator = second;
 	}
+	Fraction(const Fraction &old) {
+		this->numerator = old.numerator;
+		this->denominator = old.denominator;
+	}
+	~Fraction() //Деструктор
+	{
+		printf_s("Destructor\n");
+	}
+
 	void Initial(int first, int second){
 		numerator = first;
 		denominator = second;
@@ -19,16 +30,30 @@ public:
 	void Show(){
 		printf_s("Fraction: %d/%d\n", numerator, denominator);
 	}
-	~Fraction() //Деструктор
-	{
-		printf_s("Destructor\n");
+
+	Fraction& operator + (const Fraction& old, int a) {
+		this->numerator = old.numerator + a;
 	}
 };
+
+//Fraction& operator+ (const Fraction& first, const Fraction& second) {
+//	
+//
+//	return *temp;
+//}
 
 int main()
 {
 	Fraction firstFraction(5, 4);
+	Fraction copyFraction(firstFraction);
+
+	Fraction plusFraction = firstFraction + copyFraction;
+
 	firstFraction.Show();
+	copyFraction.Show();
+	plusFraction.Show();
+
+
 	firstFraction.Initial(2, 3);
 	firstFraction.Show();
 
